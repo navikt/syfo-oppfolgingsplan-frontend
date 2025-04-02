a8ca2dc172b22e9392c41ad9ff5966737c6e260b
Audun Sørheim <audun.sorheim@nav.no>
2025-04-02 08:56:31 +0200

    Bump tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    'postcss-import': {},
-    tailwindcss: {},
-    autoprefixer: {},
+    "@tailwindcss/postcss": {},
   },
-}
+};
 
-export default config
+export default config;
