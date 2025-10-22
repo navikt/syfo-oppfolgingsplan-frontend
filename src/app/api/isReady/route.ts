633a96b8cc1f526cc508d2dc63ecdd6442f37927
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2025-10-22 11:19:37 +0200

    Use standard response interface in route handlers

--- src/app/api/isReady/route.ts
+++ src/app/api/isReady/route.ts
@@ -1,14 +1,13 @@
-import { NextResponse } from "next/server";
 import { logger } from "@navikt/next-logger";
 import { getServerEnv } from "@/env-variables/serverEnv";
 
-export async function GET(): Promise<NextResponse> {
+export async function GET() {
   try {
     getServerEnv();
   } catch (e) {
     logger.error(e);
-    return NextResponse.json({ message: "I am not ready :(" }, { status: 500 });
+    return Response.json({ message: "I am not ready :(" }, { status: 500 });
   }
 
-  return NextResponse.json({ message: "I am ready :)" });
+  return Response.json({ message: "I am ready :)" });
 }
