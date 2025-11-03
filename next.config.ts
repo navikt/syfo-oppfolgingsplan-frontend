b2cb8056b9f308cce77339cba8ff549392d1a286
Håkon Møller <hakon.salomonsen.moller@nav.no>
2025-11-03 16:56:36 +0100

    Use React compiler (#392)
    https://nextjs.org/docs/app/guides/upgrading/version-16#react-compiler-support

--- next.config.ts
+++ next.config.ts
@@ -55,6 +55,7 @@ const nextConfig: NextConfig = {
   },
 
   reactStrictMode: true,
+  reactCompiler: true,
   output: "standalone",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX ?? "",
