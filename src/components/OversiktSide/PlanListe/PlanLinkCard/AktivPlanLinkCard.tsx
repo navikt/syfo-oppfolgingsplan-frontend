cc0e3492dc47703e68101b8461771afae034ca04
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2025-11-17 21:19:03 +0100

    Oversikt side link card adjustments

--- src/components/OversiktSide/PlanListe/PlanLinkCard/AktivPlanLinkCard.tsx
+++ src/components/OversiktSide/PlanListe/PlanLinkCard/AktivPlanLinkCard.tsx
@@ -1,33 +1,63 @@
+import NextLink from "next/link";
+import { BodyShort, LinkCard } from "@navikt/ds-react";
+import {
+  LinkCardAnchor,
+  LinkCardDescription,
+  LinkCardFooter,
+  LinkCardTitle,
+} from "@navikt/ds-react/LinkCard";
 import { FerdigstiltPlanMetadata } from "@/schema/ferdigstiltPlanMetadataSchema";
-import PlanLinkCard from "./PlanLinkCard";
-import PlanLinkCardDelingStatusFooterTags from "./PlanLinkCardFooterTags";
+import {
+  getLocaleDateAndTimeString,
+  getLocaleDateString,
+} from "@/ui-helpers/dateAndTime";
+import PlanDelingStatusTags from "./PlanLinkCardFooterTags";
 
 interface Props {
   aktivPlan: FerdigstiltPlanMetadata;
-  arbeidsstedNavn: string;
+  linkCardTitle: string;
   href: string;
 }
 
 export default function AktivPlanLinkCard({
-  aktivPlan,
-  arbeidsstedNavn,
+  aktivPlan: {
+    ferdigstiltTidspunkt,
+    evalueringsDato,
+    deltMedLegeTidspunkt,
+    deltMedVeilederTidspunkt,
+  },
+  linkCardTitle,
   href,
 }: Props) {
   return (
-    <PlanLinkCard
-      href={href}
-      planMetadata={aktivPlan}
-      arbeidsstedNavn={arbeidsstedNavn}
-      footerContent={
-        <PlanLinkCardDelingStatusFooterTags
-          isDeltMedLege={aktivPlan.deltMedLegeTidspunkt !== null}
-          isDeltMedVeileder={aktivPlan.deltMedVeilederTidspunkt !== null}
+    <LinkCard className="bg-ax-bg-success-soft">
+      <LinkCardTitle>
+        <LinkCardAnchor asChild>
+          <NextLink href={href}>{linkCardTitle}</NextLink>
+        </LinkCardAnchor>
+      </LinkCardTitle>
+
+      <LinkCardDescription>
+        <BodyShort size="small" className="mb-1">
+          Ferdigstilt {getLocaleDateAndTimeString(ferdigstiltTidspunkt, "long")}
+          .
+        </BodyShort>
+
+        <BodyShort size="small">
+          Evaluering av planen planlagt{" "}
+          {getLocaleDateString(evalueringsDato, "long")}.
+        </BodyShort>
+      </LinkCardDescription>
+
+      <LinkCardFooter>
+        <PlanDelingStatusTags
+          tagSize="small"
+          isDeltMedLege={deltMedLegeTidspunkt !== null}
+          isDeltMedVeileder={deltMedVeilederTidspunkt !== null}
           tagVariantHvisDelt="success-moderate"
           tagVariantHvisIkkeDelt="neutral-moderate"
-          tagSize="small"
         />
-      }
-      className="bg-ax-bg-success-soft"
-    />
+      </LinkCardFooter>
+    </LinkCard>
   );
 }
