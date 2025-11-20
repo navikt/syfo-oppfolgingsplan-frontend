e5e16ff4b3c1e0be96a55a0fe888d462dadd636d
Håkon Salomonsen Møller <hakon.salomonsen.moller@nav.no>
2025-11-20 10:39:13 +0100

    Modal for å overskrive utkast ved endring av plan

---
+++ src/components/FerdigstiltPlanSider/AktivPlanSide/Buttons/SlettUtkastVedOppdaterPlanModal.tsx
@@ -0,0 +1,35 @@
+import { BodyLong, Button, Modal } from "@navikt/ds-react";
+
+interface Props {
+  ref: React.RefObject<HTMLDialogElement | null>;
+}
+
+export function SlettUtkastVedOppdaterPlanModal({ ref }: Props) {
+  return (
+    <Modal
+      ref={ref}
+      header={{
+        heading: "Slett utkast?",
+        closeButton: false,
+      }}
+      closeOnBackdropClick
+    >
+      <Modal.Body>
+        <BodyLong>
+          Du har allerede et utkast. Hvis du fortsetter vil utkastet ditt bli
+          slettet og erstattet med en kopi av denne planen. Vil du fortsette?
+        </BodyLong>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button type="submit" variant="primary">
+          Slett utkast og fortsett
+        </Button>
+
+        <Button variant="secondary" onClick={() => ref.current?.close()}>
+          Behold utkastet mitt
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
