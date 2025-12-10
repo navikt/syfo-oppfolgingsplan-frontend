614e244fb69366513627ae5bc8ea5b97774a9009
Daniel Engebretsen <daniel.engebretsen@nav.no>
2025-12-10 13:46:36 +0100

    AnsattIkkeSykmeldtAlert

--- src/app/[narmesteLederId]/page.tsx
+++ src/app/[narmesteLederId]/page.tsx
@@ -1,15 +1,19 @@
 import { Suspense } from "react";
 import { BodyLong, Heading } from "@navikt/ds-react";
+import { AnsattIkkeSykmeldtAlert } from "@/components/OversiktSide/AnsattIkkeSykmeldtAlert.tsx";
 import OversiktSideInformasjon from "@/components/OversiktSide/InformasjonSection/OversiktSideInformasjon";
 import NyPlanButtonHvisTomListe from "@/components/OversiktSide/PlanListe/NyPlanButtonHvisTomListe";
 import PlanListeForArbeidsgiver from "@/components/OversiktSide/PlanListe/PlanListeForArbeidsgiver";
 import PlanListeSkeleton from "@/components/OversiktSide/PlanListe/PlanListeSkeleton";
 import TextContentBox from "@/components/layout/TextContentBox";
+import { fetchOppfolgingsplanOversiktForAG } from "@/server/fetchData/arbeidsgiver/fetchOppfolgingsplanOversikt.ts";
 
 export default async function OversiktPageForAG({
   params,
 }: PageProps<"/[narmesteLederId]">) {
   const { narmesteLederId } = await params;
+  const { userHasEditAccess } =
+    await fetchOppfolgingsplanOversiktForAG(narmesteLederId);
 
   return (
     <>
@@ -24,6 +28,8 @@ export default async function OversiktPageForAG({
           at den ansatte kan komme tilbake i arbeid.
         </BodyLong>
       </TextContentBox>
+
+      {!userHasEditAccess && <AnsattIkkeSykmeldtAlert />}
 
       <Suspense fallback={<PlanListeSkeleton />}>
         <NyPlanButtonHvisTomListe narmesteLederId={narmesteLederId} />
---
+++ src/components/OversiktSide/AnsattIkkeSykmeldtAlert.tsx
@@ -0,0 +1,21 @@
+import { Alert, BodyShort, Box, Heading } from "@navikt/ds-react";
+
+export function AnsattIkkeSykmeldtAlert() {
+  return (
+    <Box>
+      <Alert variant="info" size="small" className="mb-12">
+        <Heading level="3" size="small" spacing>
+          Den ansatte er ikke sykmeldt
+        </Heading>
+        <Box>
+          <BodyShort>
+            Du kan ikke opprette en oppfølgingsplan for den ansatte nå. Du kan
+            kun opprette nye oppfølgingsplaner når den ansatte har en
+            sykmelding, eller det er mindre enn 16 dager siden siste
+            sykmeldingsdato.
+          </BodyShort>
+        </Box>
+      </Alert>
+    </Box>
+  );
+}
