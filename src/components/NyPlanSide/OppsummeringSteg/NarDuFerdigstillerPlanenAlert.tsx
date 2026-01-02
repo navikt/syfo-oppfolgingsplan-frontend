a6386278a418bd7d044ad0ede346623fcdd282ab
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2026-01-02 13:21:58 +0100

    Minor text adjustment for sharing with employee (#527)

--- src/components/NyPlanSide/OppsummeringSteg/NarDuFerdigstillerPlanenAlert.tsx
+++ src/components/NyPlanSide/OppsummeringSteg/NarDuFerdigstillerPlanenAlert.tsx
@@ -12,12 +12,12 @@ export default function NarDuFerdigstillerPlanenAlert({ className }: Props) {
       </Heading>
 
       <BodyLong>
-        Når du velger å ferdigstille planen deles den med den ansatte, og de kan
-        finne den ved å logge seg på nav.no. Du som arbeidsgiver vil også finne
-        planen under Oppfølgingsplaner på nav.no. Hvis du ikke er klar til å
-        dele planen med den ansatte enda, så kan du avslutte og fortsette
-        senere. Det du har skrevet finner du igjen i et utkast under
-        oppfølgingsplaner for den ansatte.
+        Når du velger å ferdigstille planen deles den med den ansatte.
+        Arbeidstakeren finner planen ved å logge seg på nav.no. Du som
+        arbeidsgiver vil også finne planen under Oppfølgingsplaner på nav.no.
+        Hvis du ikke er klar til å dele planen med den ansatte enda, så kan du
+        avslutte og fortsette senere. Det du har skrevet finner du igjen i et
+        utkast under oppfølgingsplaner for den ansatte.
       </BodyLong>
     </Alert>
   );
