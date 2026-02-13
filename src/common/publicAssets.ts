f47af10796c92d72029fd01d6beee21eb9022fd0
AudunSorheim <80095835+AudunSorheim@users.noreply.github.com>
2026-02-13 12:31:59 +0100

    Use cdn for build artifacts and public files (#584)

---
+++ src/common/publicAssets.ts
@@ -0,0 +1,14 @@
+import { publicAssetUrl } from "@/common/publicAssetUrl";
+
+export const ERROR_PAGE_DAD_SVG = publicAssetUrl(
+  "/illustrations/error-page-dad.svg",
+);
+export const PENCIL_SVG = publicAssetUrl("/illustrations/pencil.svg");
+export const LETTER_OPENED_SVG = publicAssetUrl(
+  "/illustrations/letter-opened.svg",
+);
+export const STEPPER_SVG = publicAssetUrl("/illustrations/stepper.svg");
+export const CLIPBOARD_SVG = publicAssetUrl("/illustrations/clipboard.svg");
+export const CHAT_BUBBLE_SVG = publicAssetUrl(
+  "/illustrations/chat-double-bubble.svg",
+);
